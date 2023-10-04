97dad43d015fa37f7a4a45463e5df3a81cdd213d
etopritika <dimaprytyka@gmail.com>
2023-10-04 16:04:48 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+In this application, I configured the renderer with a React.memo and hook React.useMemo to prevent unnecessary rerendering of components.
---
+++ src/components/IsFive.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+let renderCount = 0;
+
+export default React.memo(
+  function IsFive({ value }) {
+    console.warn(`🔴 isFive render: ${++renderCount}`);
+
+    const getResult = React.useMemo(() => {
+      let i = 0;
+      while (i < 600000000) i++;
+      return value === 5 ? '✅ This is five :D' : '❌ Not five :(';
+    },[value])
+
+    return <h3>{getResult}</h3>;
+  },
+  (prevProps, nextPropx) => {
+    if (nextPropx.value === 5) {
+      return false;
+    } else {
+      return true;
+    }
+  },
+);
